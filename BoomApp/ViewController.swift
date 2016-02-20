9d286dbc2314bf53345f69654d1ea0a9624ce798
Marinao Darrosier <marinaodarrosier@Marinaos-MBP.attlocal.net>
2016-02-19 23:27:37 -0500

    added first files

--- BoomApp/ViewController.swift
+++ BoomApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgBoca: UIImageView!
+    @IBOutlet var imgRiverPlate: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnRiverPlate(sender: AnyObject) {
+        imgRiverPlate.hidden = true
+    }
 
+    @IBAction func btnBocaJr(sender: AnyObject) {
+        
+        imgBoca.hidden = true
+    }
+    @IBAction func btnReset(sender: AnyObject) {
+        imgBoca.hidden = false
+        imgRiverPlate.hidden = false
+    }
 }
 
